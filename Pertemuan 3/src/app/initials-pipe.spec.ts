feedc3f7b613685d8562d2e9589721fe6084b8fc
Jenny CHang <ajjajeni12@gmail.com>
2026-09-04 21:20:08 +0700

    Add Pertemuan 3

---
+++ Pertemuan 3/src/app/initials-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { InitialPipe } from './initials-pipe';
+
+describe('InitialPipe', () => {
+  it('create an instance', () => {
+    const pipe = new InitialPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
